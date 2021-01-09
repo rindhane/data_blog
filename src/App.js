1ac04e1cd4054659ec3958e78c272aeccd9b69af
irahul <rindhane@users.noreply.github.com>
2021-01-10 04:47:27 +0530

    basic commit of react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Project from "./project.js";
+import './App.css';
+
+function App() {
+
+  return (
+      <div >
+        <h1>Hello</h1>
+        <h2>well</h2>
+        <Project />
+    </div>
+  );
+}
+
+export default App;
